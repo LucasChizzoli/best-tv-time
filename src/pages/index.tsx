c0c988f4f21afe6c72a6cc7ae4ee2df237a99b06
LucasChizzoli <chizzoli.lucas13@gmail.com>
2022-06-11 20:54:57 -0300

    add html template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Head>
+        <title>Best Tv Time</title>
+        <meta name="description" content="Rate the best movie and tv-show"/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </main>
+    </div>
   )
 }
 
