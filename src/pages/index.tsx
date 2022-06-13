e5bfc6971b68a712b274fa9aa4df74251d1f1403
LucasChizzoli <chizzoli.lucas13@gmail.com>
2022-06-13 20:39:08 -0300

    call movies api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from 'next';
+import { getOptionsForVote } from '../utils/getRandomMovie';
 import { trpc } from '../utils/trcp';
 
 const Home: NextPage = () => {
+  const {data, isLoading} = trpc.useQuery(["media.get-movie-by-id", { id: 550 }]);
+  
+  if (isLoading || !data) {
+    return <div>Loading...</div>
+  }
 
-  // const { data, isLoading } = trpc.useQuery(["media.get-all"]);
-
-  // if (isLoading || !data) {
-  //   return <div>Loading...</div>
-  // }
+  console.log(data);
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
@@ -15,9 +17,9 @@ const Home: NextPage = () => {
         Which movie/tv-show is better?
       </h1>
       <div className="mt-5 flex justify-between max-w-2xl">
-        
+        <div className="w-16 h-16 bg-red-200">{data?.title}</div>
         <div className="p-8">vs</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-200">{data?.title}</div>
       </div>
     </div>
   )
