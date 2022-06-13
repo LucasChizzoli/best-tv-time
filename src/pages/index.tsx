7300e45794ffd3cc137c02aa25222d4a79a398ee
LucasChizzoli <chizzoli.lucas13@gmail.com>
2022-06-12 21:34:53 -0300

    Create prisma schema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
       </Head>
       <main>
         <h1 className="text-3xl font-bold underline">
-          Hello world!
+          Hello world
         </h1>
       </main>
     </div>
